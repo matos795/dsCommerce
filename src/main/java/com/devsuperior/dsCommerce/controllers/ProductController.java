7878b25a10ba14d1f7c95325f5d9187e39c62af7
Alexandre Matos <matosalexandre795@gmail.com>
2025-08-28 16:56:23 -0300

    New -> Repository: ProductRepository & Tests!

--- src/main/java/com/devsuperior/dsCommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dsCommerce/controllers/ProductController.java
@@ -1,15 +1,26 @@
 package com.devsuperior.dsCommerce.controllers;
 
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsCommerce.entities.Product;
+import com.devsuperior.dsCommerce.repositories.ProductRepository;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository repository;
     @GetMapping
     public String teste(){
-        return "Teste";
+
+       Optional<Product> obj = repository.findById(1L);
+       return obj.get().getName();
     }
 }
