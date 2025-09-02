58e8cc435fc8f5ef2503365b9294d8acbed35d12
Alexandre Matos <matosalexandre795@gmail.com>
2025-09-02 11:42:32 -0300

    Update Controller: POST new product!

--- src/main/java/com/devsuperior/dsCommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dsCommerce/services/ProductService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,8 +26,19 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(org.springframework.data.domain.Pageable pageable) {
+    public Page<ProductDTO> findAll(Pageable pageable) {
         Page<Product> obj = repository.findAll(pageable);
         return obj.map(x -> new ProductDTO(x));
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
     }
 }
