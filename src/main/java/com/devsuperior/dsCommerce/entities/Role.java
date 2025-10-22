30a13a22d810c395f332d65f676c80dafe1ab35e
Alexandre Matos <matosalexandre795@gmail.com>
2025-10-22 11:20:48 -0300

    UserDetails, GrantedAuthority!

--- src/main/java/com/devsuperior/dsCommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dsCommerce/entities/Role.java
@@ -3,6 +3,8 @@ package com.devsuperior.dsCommerce.entities;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,7 +16,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,6 +44,8 @@ public class Role {
     public void setId(Long id) {
         this.id = id;
     }
+
+    @Override
     public String getAuthority() {
         return authority;
     }
